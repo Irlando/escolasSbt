03567d240ac5b32cbead651912a4f601c3885c8f
irlando <ijirlando@gmail.com>
2025-01-01 19:03:30 -0100

    update lista da licao

--- src/data/lessons.ts
+++ src/data/lessons.ts
@@ -3,25 +3,25 @@ import { Lesson } from '../types/lesson';
 export const lessons: Lesson[] = [
   {
     id: '1',
-    number: '04',
+    number: '01',
     title: 'O EVANGELHO DE JOÃO',
-    subtitle: '4º Trimestre - 2024',
+    subtitle: '1º Trimestre - 2025',
     imageUrl: 'https://images.unsplash.com/photo-1504052434569-70ad5836ab65?auto=format&fit=crop&q=80',
     weekTheme: 'A Palavra se fez carne'
   },
   {
     id: '2',
-    number: '05',
-    title: 'O TESTEMUNHO DE JOÃO',
-    subtitle: '4º Trimestre - 2024',
+    number: '02',
+    title: 'O TESTEMUNHO DE JOÃßO',
+    subtitle: '1º Trimestre - 2025',
     imageUrl: 'https://images.unsplash.com/photo-1507434965515-61970f2bd7c6?auto=format&fit=crop&q=80',
     weekTheme: 'O Cordeiro de Deus'
   },
   {
     id: '3',
-    number: '06',
+    number: '03',
     title: 'OS SINAIS DE JESUS',
-    subtitle: '4º Trimestre - 2024',
+    subtitle: '1º Trimestre - 2025',
     imageUrl: 'https://images.unsplash.com/photo-1499209974431-9dddcece7f88?auto=format&fit=crop&q=80',
     weekTheme: 'Milagres e Fé'
   }
